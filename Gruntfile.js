ed3b72dc8494d71ca6462e5d6ae1fecce7d9e5cc
Béla Varga <netzzwerg@googlemail.com>
2013-03-20 00:12:15 +0100

    Added working example and changed config.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,24 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
 
-
-
     jasmine : {
       src : 'src/**/*.js',
       options : {
-        specs : 'spec/**/*.js',
-        template: require('grunt-template-jasmine-requirejs')
+        verbose: true,
+        specs: 'spec/**/*Spec.js',
+        helpers: 'spec/**/*Helper.js',
+        template: require('grunt-template-jasmine-requirejs'),
+        templateOptions: {
+          requireConfig: {
+            baseUrl: ''
+          }
+        }
       }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-
-
   grunt.registerTask('test', ['jasmine']);
 
 };
--- src/Foo.js
+++ src/Foo.js
@@ -3,6 +3,12 @@ define([], function () {
 	var exports = {
 		init:function () {
 			return true;
+		},
+		get: function () {
+			return 12345;
+		},
+		set: function () {
+			return true;
 		}
 	};
 
