1fefafa43f3df3aca6f5f0c8ec6bb804101fc2cd
Béla Varga <netzzwerg@googlemail.com>
2013-03-20 00:12:15 +0100

    Added working example and changed config.

--- spec/FooSpec.js
+++ spec/FooSpec.js
@@ -1,9 +1,20 @@
-define(['Foo'], function(Foo){
+define(['src/Foo'], function(Foo){
 
 	describe("FooSpec", function() {
 
-		it("should be an example for breaking test", function() {
-			expect(false).toBeFalsy();
+		it("should be an example for working test", function() {
+			var result = Foo.init();
+			expect(result).toBe(true);
+		});
+
+		it("should be an example for working test", function() {
+			var result = Foo.get();
+			expect(result).toBe(12345);
+		});
+
+		it("should be an example for working test", function() {
+			var result = Foo.set();
+			expect(result).toBe(true);
 		});
 
 	});
